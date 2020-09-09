7e756b0bd53d39b7fe57a59d0e77deb4c65747dc
Bikram Parajuli <39263700+Parajulibkrm@users.noreply.github.com>
2020-09-09 18:25:16 +0545

    Added environment port support

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mainRoutes = require("./routes/mainRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 require('./db/mongoose.js')
 const app = express();
-const port = 8080;
+const port = process.env.port || 8080;
 
 
 app.use(express.json());
