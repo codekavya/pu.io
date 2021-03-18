125267cdcaeacc06598bf7b0da41e9986e2d2b72
Parajulibkrm <bikramparajuli0000@gmail.com>
2021-03-18 10:32:38 +0545

    remaining refactor and gitignored puio.zip

--- Utils/__cookies__extracter.js
+++ Utils/__cookies__extracter.js
@@ -1,11 +1,12 @@
-exports.parseCookies  = (request)=> {
-    var list = {},
-        reqList = request.headers.cookie;
+export function parseCookies(request) {
+  var list = {},
+    reqList = request.headers.cookie;
 
-    reqList && reqList.split(';').forEach(function( cookie ) {
-        var parts = cookie.split('=');
-        list[parts.shift().trim()] = decodeURI(parts.join('='));
+  reqList &&
+    reqList.split(";").forEach(function (cookie) {
+      var parts = cookie.split("=");
+      list[parts.shift().trim()] = decodeURI(parts.join("="));
     });
 
-    return list;
+  return list;
 }
