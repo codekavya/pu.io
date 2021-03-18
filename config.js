8199cf219558bb614d586cb38599a35852d1df3b
Bikram Parajuli <39263700+Parajulibkrm@users.noreply.github.com>
2021-03-18 21:36:33 +0545

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  jwtsigninKey: "some random stuff",
-  mongoDbKey:
-    "mongodb+srv://codekavya:wealreadychangedpassword@cluster0.lwyca.mongodb.net/pu?retryWrites=true&w=majority",
-  localKey: "mongodb://127.0.0.1:27017/users",
-};
