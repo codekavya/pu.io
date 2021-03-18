31a734c5b960e48145ea452f4b76540dfca82bf3
Bikram Parajuli <39263700+Parajulibkrm@users.noreply.github.com>
2021-03-18 21:16:47 +0545

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-  jwtsigninKey: "codekavyaandmakers$$$",
+  jwtsigninKey: "some random stuff",
   mongoDbKey:
     "mongodb+srv://codekavya:wealreadychangedpassword@cluster0.lwyca.mongodb.net/pu?retryWrites=true&w=majority",
   localKey: "mongodb://127.0.0.1:27017/users",
