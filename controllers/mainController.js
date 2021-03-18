45ad89d72717cde71df1584e8b5c731a5788fb2c
Parajulibkrm <bikramparajuli0000@gmail.com>
2021-03-18 20:55:39 +0545

    Handled promise with async await in getPrograms

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -10,23 +10,25 @@ import apiCounts from "../models/apiModels.js";
 import { getNotices, getNoticeContent } from "../Services/notices.js";
 
 export async function getPrograms(req, res, next) {
-  req.user.requestCount += 1;
-  await req.user.save();
-  programs
-    .find()
-    .then((programs) => {
-      res.status(200).json({
-        programs: programs,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+  try {
+    const programList = await programs.find({});
+    let count = null;
+    console.log(req.api);
+    if (req.user.apiKey && req.api) {
+      const api = await apiCounts.findOne({ Apikey: req.user.apikey });
+      // console.log({ api });
+      api.TodayHits = api.TodayHits + 1;
+      api.TotalHits = api.TotalHits + 1;
+      api.save();
+      count = api.TodayHits;
+    }
+    await req.user.save();
+    res.send({ programList, count });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 }
-
 export async function getSchoolsandcolleges(req, res, next) {
   try {
     const schoolsandcollege = await schoolsandcolleges.find({});
