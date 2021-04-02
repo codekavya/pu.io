7898b559e75d0e452ec1924909b7f21e73316575
Parajulibkrm <bikramparajuli0000@gmail.com>
2021-04-02 11:51:10 +0545

    minor merge fixes with collection name

--- models/chatModel.js
+++ models/chatModel.js
@@ -3,20 +3,20 @@ const { Schema: _Schema, model } = mongoose;
 const Schema = _Schema;
 
 //This schema defines the way individual chat is stored in database
-  const chatSchema = new Schema({
-    message: String,
-    messageBy:{
-      type: Schema.Types.ObjectId,
-      ref:"Admin"
-    },
-    chatRoomName:{
-      type:Schema.Types.ObjectId,
-      ref:"chatRoom"
-    },
-    messageSentAt:{
-      type:Date
-    }
-  });
-  const MessageModel = model("chats", chatSchema);
+const chatSchema = new Schema({
+  message: String,
+  messageBy: {
+    type: Schema.Types.ObjectId,
+    ref: "admins",
+  },
+  chatRoomName: {
+    type: Schema.Types.ObjectId,
+    ref: "chatRoom",
+  },
+  messageSentAt: {
+    type: Date,
+  },
+});
+const MessageModel = model("chats", chatSchema);
 
 export default MessageModel;
--- models/chatRoomModel.js
+++ models/chatRoomModel.js
@@ -1,31 +1,35 @@
 import mongoose from "mongoose";
-const {
-  Schema: _Schema,
-  model
-} = mongoose;
+const { Schema: _Schema, model } = mongoose;
 const Schema = _Schema;
 
 //This schema defines the way individual chat is stored in database
-const chatSchema = new Schema({
-  Name: {
-    type: String,
-    unique: true["The Room Name Already Exists"]
+const chatSchema = new Schema(
+  {
+    Name: {
+      type: String,
+      unique: true["The Room Name Already Exists"],
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "admins",
+    },
+    RoomMembers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "admins",
+      },
+    ],
+    messages: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "chats",
+      },
+    ],
   },
-  createdBy: {
-    type: Schema.Types.ObjectId,
-    ref:"Admin"
-  },
-  RoomMembers: [{
-    type: Schema.Types.ObjectId,
-    ref: "Admin"
-  }],
-  messages: [{
-    type: Schema.Types.ObjectId,
-    "ref": "chats"
-  }]
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 const chatModel = model("chatRoom", chatSchema);
 
 export default chatModel;
