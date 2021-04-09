f8295dc39f6852a0f043d6d56d47210b2ae6a801
Parajulibkrm <bikramparajuli0000@gmail.com>
2021-04-09 10:53:57 +0545

    username implemented in club controller, relationship implemented

--- controllers/mainControllers/clubsController.js
+++ controllers/mainControllers/clubsController.js
@@ -3,7 +3,7 @@ const { Router } = express;
 import clubs from "../../models/clubsinfo.js";
 import checkRole from "../../auth/checkRole.js";
 const router = Router();
-
+import users from "../../models/adminModels.js";
 
 //TODO:Auth needed to be implemented
 export async function getClubs(req, res) {
@@ -12,7 +12,7 @@ export async function getClubs(req, res) {
     res.send({ clubs: clubsList, count: req.count });
   } catch (error) {
     console.log(error);
-    res.status(500).send(error);
+    return res.status(500).send({ Error: error });
   }
 }
 export async function getClub(req, res) {
@@ -21,29 +21,53 @@ export async function getClub(req, res) {
     res.send({ club, count: req.count });
   } catch (error) {
     console.log(error);
-    res.status(500).send(error);
+    return res.status(500).send({ Error: error });
   }
 }
 
 export async function createClub(req, res) {
-  const club = new clubs(req.body);
   try {
-    await club.save();
-    res.send({ club });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
+    let committeeMembers = [];
+    const members = await req.body.committee.map(async (element) => {
+      const member = await users.findOne({ Username: element.member });
+      if (!member) {
+        throw new Error(`${element.member} doesn't exit.`);
+      }
+      if (element.canPostEvent === true) {
+        member.roles.push("role.clubManager");
+      }
+      committeeMembers.push(member);
+      return { ...element, member: member._id };
+    });
+    const committee = [...(await Promise.all(members))];
+    const club = await new clubs({
+      ...req.body,
+      committee,
+    });
+    try {
+      await club.save();
+      committeeMembers.forEach((member) => {
+        member.clubs.push(club._id);
+        member.save();
+      });
+      return res.send({ club });
+    } catch (error) {
+      return res.status(500).send({ Error: error });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(404).send({ Error: err.message.toString() });
   }
 }
 export async function deleteClub(req, res) {
   try {
     const club = await clubs.findByIdAndDelete(req.params.id);
 
-    if (!club) res.status(404).send("No items Found");
+    if (!club) return res.status(404).send("No items Found");
     res.send({ message: "Club deleted" });
   } catch (error) {
     console.log(error);
-    res.status(500).send(error);
+    return res.status(500).send({ Error: error });
   }
 }
 export async function updateClub(req, res) {
@@ -54,7 +78,7 @@ export async function updateClub(req, res) {
     res.send({ club });
   } catch (error) {
     console.log(error);
-    res.status(500).send(error);
+    return res.status(500).send({ Error: error });
   }
 }
 router.get("/", getClubs);
